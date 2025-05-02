191df3fbf0c7c33cec95996a3e419a9add5a7a20
t0yohei <inuneko690121@gmail.com>
2025-05-02 18:49:14 +0900

    nits: add decrement button

--- examples/counter/index.rb
+++ examples/counter/index.rb
@@ -1,9 +1,19 @@
 require "js"
 
+actions = {
+  increment: ->(state, _payload) {
+    { count: state[:count] + 1 }
+  },
+  decrement: ->(state, _payload) {
+    { count: state[:count] - 1 }
+  }
+}
+
 view = ->(state, emit) {
   RubyWasmUi::Vdom.h("div", {}, [
     RubyWasmUi::Vdom.h("div", {}, [state[:count]]),
-    RubyWasmUi::Vdom.h("button", { onclick: ->(e) { emit.call(:increment) } }, ["Increment"])
+    RubyWasmUi::Vdom.h("button", { onclick: ->(e) { emit.call(:increment) } }, ["Increment"]),
+    RubyWasmUi::Vdom.h("button", { onclick: ->(e) { emit.call(:decrement) } }, ["Decrement"])
   ])
 }
 
@@ -13,11 +23,7 @@ app_a = RubyWasmUi::App.create(
     count: 0
   },
   view:,
-  actions: {
-    increment: ->(state, payload) {
-      { count: state[:count] + 1 }
-    }
-  }
+  actions:
 )
 app_element_a = JS.global[:document].getElementById("app-a")
 app_a.mount(app_element_a)
@@ -29,11 +35,7 @@ app_b = RubyWasmUi::App.create(
     count: 0
   },
   view:,
-  actions: {
-    increment: ->(state, payload) {
-      { count: state[:count] + 1 }
-    }
-  }
+  actions:
 )
 app_element_b = JS.global[:document].getElementById("app-b")
 app_b.mount(app_element_b)
