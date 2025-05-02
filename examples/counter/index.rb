b752ed21955580c74006e22aea61cead298fbbca
t0yohei <inuneko690121@gmail.com>
2025-05-02 18:38:03 +0900

    refactor: separate hello and counter examples

---
+++ examples/counter/index.rb
@@ -0,0 +1,39 @@
+require "js"
+
+view = ->(state, emit) {
+  RubyWasmUi::Vdom.h("div", {}, [
+    RubyWasmUi::Vdom.h("div", {}, [state[:count]]),
+    RubyWasmUi::Vdom.h("button", { onclick: ->(e) { emit.call(:increment) } }, ["Increment"])
+  ])
+}
+
+# app_a to be destroyed
+app_a = RubyWasmUi::App.create(
+  state: {
+    count: 0
+  },
+  view:,
+  reducers: {
+    increment: ->(state, payload) {
+      { count: state[:count] + 1 }
+    }
+  }
+)
+app_element_a = JS.global[:document].getElementById("app-a")
+app_a.mount(app_element_a)
+app_a.unmount
+
+# app_b to be mounted
+app_b = RubyWasmUi::App.create(
+  state: {
+    count: 0
+  },
+  view:,
+  reducers: {
+    increment: ->(state, payload) {
+      { count: state[:count] + 1 }
+    }
+  }
+)
+app_element_b = JS.global[:document].getElementById("app-b")
+app_b.mount(app_element_b)
