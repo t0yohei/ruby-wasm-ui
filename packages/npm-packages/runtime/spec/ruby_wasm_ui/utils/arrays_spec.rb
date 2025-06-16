0829d7674012e18b5850990e0e05df948e4fae63
t0yohei <inuneko690121@gmail.com>
2025-06-16 21:01:35 +0900

    feat: add reconciliation algorithm

--- packages/npm-packages/runtime/spec/ruby_wasm_ui/utils/arrays_spec.rb
+++ packages/npm-packages/runtime/spec/ruby_wasm_ui/utils/arrays_spec.rb
@@ -38,4 +38,126 @@ RSpec.describe RubyWasmUi::Utils::Arrays do
       end
     end
   end
+
+  describe '.diff' do
+    context 'Basic functionality' do
+      it 'correctly detects added and removed elements' do
+        old_array = [1, 2, 3]
+        new_array = [2, 3, 4]
+        result = described_class.diff(old_array, new_array)
+        expect(result).to eq({
+          added: [4],
+          removed: [1]
+        })
+      end
+
+      it 'returns empty arrays when comparing identical arrays' do
+        array = [1, 2, 3]
+        result = described_class.diff(array, array)
+        expect(result).to eq({
+          added: [],
+          removed: []
+        })
+      end
+    end
+
+    context 'Edge cases' do
+      it 'works correctly when comparing with an empty array' do
+        old_array = []
+        new_array = [1, 2, 3]
+        result = described_class.diff(old_array, new_array)
+        expect(result).to eq({
+          added: [1, 2, 3],
+          removed: []
+        })
+      end
+
+      it 'handles duplicate elements correctly' do
+        old_array = [1, 1, 2, 2]
+        new_array = [2, 2, 3, 3]
+        result = described_class.diff(old_array, new_array)
+        expect(result).to eq({
+          added: [3],
+          removed: [1]
+        })
+      end
+    end
+  end
+
+  describe '.diff_sequence' do
+    context 'Basic functionality' do
+      it 'returns correct sequence for simple addition' do
+        old_array = [1, 2, 3]
+        new_array = [1, 2, 3, 4]
+        result = described_class.diff_sequence(old_array, new_array)
+        expect(result).to eq([
+          { op: 'noop', original_index: 0, index: 0, item: 1 },
+          { op: 'noop', original_index: 1, index: 1, item: 2 },
+          { op: 'noop', original_index: 2, index: 2, item: 3 },
+          { op: 'add', index: 3, item: 4 }
+        ])
+      end
+
+      it 'returns correct sequence for simple removal' do
+        old_array = [1, 2, 3, 4]
+        new_array = [1, 2, 3]
+        result = described_class.diff_sequence(old_array, new_array)
+        expect(result).to eq([
+          { op: 'noop', original_index: 0, index: 0, item: 1 },
+          { op: 'noop', original_index: 1, index: 1, item: 2 },
+          { op: 'noop', original_index: 2, index: 2, item: 3 },
+          { op: 'remove', index: 3, item: 4 }
+        ])
+      end
+
+      it 'returns correct sequence for reordering' do
+        old_array = [1, 2, 3]
+        new_array = [3, 1, 2]
+        result = described_class.diff_sequence(old_array, new_array)
+        expect(result).to eq([
+          { op: 'move', original_index: 2, from: 2, index: 0, item: 3 },
+          { op: 'noop', original_index: 0, index: 1, item: 1 },
+          { op: 'noop', original_index: 1, index: 2, item: 2 }
+        ])
+      end
+    end
+
+    context 'Complex scenarios' do
+      it 'handles multiple operations in sequence' do
+        old_array = [1, 2, 3, 4]
+        new_array = [5, 2, 1, 6]
+        result = described_class.diff_sequence(old_array, new_array)
+        expect(result).to eq([
+          { op: 'add', index: 0, item: 5 },
+          { op: 'move', original_index: 1, from: 2, index: 1, item: 2 },
+          { op: 'noop', original_index: 0, index: 2, item: 1 },
+          { op: 'remove', index: 3, item: 3 },
+          { op: 'remove', index: 3, item: 4 },
+          { op: 'add', index: 3, item: 6 }
+        ])
+      end
+
+      it 'handles empty arrays' do
+        old_array = []
+        new_array = []
+        result = described_class.diff_sequence(old_array, new_array)
+        expect(result).to eq([])
+      end
+
+      it 'handles complete replacement' do
+        old_array = ['X', 'A', 'A', 'B', 'C']
+        new_array = ['C', 'K', 'A', 'B']
+
+        result = described_class.diff_sequence(old_array, new_array)
+        expect(result).to eq([
+          { op: 'remove', index: 0, item: 'X' },
+          { op: 'move', original_index: 4, from: 3, index: 0, item: 'C' },
+          { op: 'add', index: 1, item: 'K' },
+          { op: 'noop', original_index: 1, index: 2, item: 'A' },
+          { op: 'move', original_index: 3, from: 4, index: 3, item: 'B' },
+          { op: 'remove', index: 4, item: 'A' }
+        ])
+      end
+    end
+  end
 end
