dee034df249b3b6658f389379b288492c5ceed30
t0yohei <inuneko690121@gmail.com>
2025-06-29 11:11:37 +0900

    refactor: RubyWasmUi::Template::Parser

--- packages/npm-packages/runtime/src/ruby_wasm_ui/template/parser.rb
+++ packages/npm-packages/runtime/src/ruby_wasm_ui/template/parser.rb
@@ -5,6 +5,8 @@ module RubyWasmUi
     module Parser
       module_function
 
+      # @param template [String]
+      # @return [String]
       def parse(template)
         parser = JS.eval('return new DOMParser()')
         document = parser.call(:parseFromString, JS.try_convert(template), 'text/html')
@@ -13,56 +15,75 @@ module RubyWasmUi
         build_vdom(elements)
       end
 
+      # @param elements [JS.Array]
+      # @return [String]
       def build_vdom(elements)
         vdom = []
         elements.forEach do |element|
+          # text node
           if element[:nodeType] == JS.global[:Node][:TEXT_NODE]
-            value = element[:nodeValue].to_s.chomp.strip
-
-            next if value.empty?
-
-            vdom << if embed_script?(value)
-                      get_embed_script(value)
-                    else
-                      "'#{value}'"
-                    end
-
+            vdom << parse_text_node(element)
             next
           end
 
+          # fragment node
+          if element[:nodeType] == JS.global[:Node][:ELEMENT_NODE] && element[:tagName] == 'TEMPLATE'
+            vdom << "RubyWasmUi::Vdom.h_fragment([#{build_vdom(element[:content][:childNodes])}])"
+            next
+          end
+
+          # element node
           if element[:nodeType] == JS.global[:Node][:ELEMENT_NODE]
-            if element[:tagName] == 'TEMPLATE'
-              vdom << "RubyWasmUi::Vdom.h_fragment([#{build_vdom(element[:content][:childNodes])}])"
-              next
-            end
+            attributes = parse_attributes(element[:attributes])
+            vdom << "RubyWasmUi::Vdom.h('#{element[:tagName].to_s.downcase}', {#{attributes}}, [#{build_vdom(element[:childNodes])}])"
+            next
           end
-
-          attributes_str = []
-          attributes = element[:attributes]
-          length = attributes[:length].to_i
-          length.times do |i|
-            attribute = attributes[i]
-            key = attribute[:name].to_s
-            value = attribute[:value].to_s
-
-            result = if embed_script?(value)
-                        script = get_embed_script(value)
-                        ":#{key} => #{script}"
-                      else
-                        ":#{key} => '#{value}'"
-                      end
-            attributes_str << result
-          end
-
-          vdom << "RubyWasmUi::Vdom.h('#{element[:tagName].to_s.downcase}', {#{attributes_str.join(', ')}}, [#{build_vdom(element[:childNodes])}])"
         end
-        vdom.join(',')
+        vdom.compact.join(',')
       end
 
+      # @param element [JS.Object]
+      # @return [String]
+      def parse_text_node(element)
+        value = element[:nodeValue].to_s.chomp.strip
+
+        return nil if value.empty?
+
+        return get_embed_script(value) if embed_script?(value)
+
+        "'#{value}'"
+      end
+
+      # @param attributes [JS.Object]
+      # @return [String]
+      def parse_attributes(attributes)
+        attributes_str = []
+
+        # attributes is JS.Object that can't use ruby method like each
+        length = attributes[:length].to_i
+        length.times do |i|
+          attribute = attributes[i]
+          key = attribute[:name].to_s
+          value = attribute[:value].to_s
+
+          if embed_script?(value)
+            attributes_str << ":#{key} => #{get_embed_script(value)}"
+            next
+          end
+
+          attributes_str << ":#{key} => '#{value}'"
+        end
+        attributes_str.join(', ')
+      end
+
+      # @param doc [String]
+      # @return [Boolean]
       def embed_script?(doc)
         doc.match?(/\{.+\}/)
       end
 
+      # @param script [String]
+      # @return [String]
       def get_embed_script(script)
         script.gsub(/\{(.+)\}/) { ::Regexp.last_match(1) }
       end
