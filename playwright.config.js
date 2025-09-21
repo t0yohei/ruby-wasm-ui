a04383a88b8a08fad03cccaddd3df81a324c89c8
t0yohei <inuneko690121@gmail.com>
2025-09-21 16:47:22 +0900

    ci: enable playwrite execute parallel on CI

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { open: "never" }]],
   /* Output directory for test results */
