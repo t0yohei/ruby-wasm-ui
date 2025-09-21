86020a02014c0a247e7780296eb01f25c0665efc
t0yohei <inuneko690121@gmail.com>
2025-09-21 16:11:38 +0900

    update: locate test file for Co-location

--- playwright.config.js
+++ playwright.config.js
@@ -4,13 +4,14 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: "./ui-tests",
+  testDir: "./examples",
+  testMatch: "*/*.spec.js",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
