8622a1cc974733fd5f408e0227460757885bfa5c
t0yohei <inuneko690121@gmail.com>
2025-09-21 17:38:40 +0900

    feat: Add server start command for test

--- playwright.config.js
+++ playwright.config.js
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run serve:examples:dev",
+    command: "npm run serve:examples:test",
     url: "http://localhost:8080",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // 2 minutes timeout for Ruby WASM to load
