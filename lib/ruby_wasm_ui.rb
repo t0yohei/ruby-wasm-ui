de807ea90c4a1d2392d4b43cc1fc48c1c09b1d0b
t0yohei <inuneko690121@gmail.com>
2025-11-17 18:11:10 +0900

    update: change to symbolic link

--- lib/ruby_wasm_ui.rb
+++ lib/ruby_wasm_ui.rb
@@ -1,8 +1,1 @@
-# frozen_string_literal: true
-
-require_relative "ruby_wasm_ui/version"
-
-module RubyWasmUi
-  class Error < StandardError; end
-  # Your code goes here...
-end
+../packages/npm-packages/runtime/src/ruby_wasm_ui.rb
