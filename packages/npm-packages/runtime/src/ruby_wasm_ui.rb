a0c80eb89177b2ba150113f902587d84a90fbd91
t0yohei <inuneko690121@gmail.com>
2025-11-17 18:30:20 +0900

    update: files to symbolic links

--- packages/npm-packages/runtime/src/ruby_wasm_ui.rb
+++ packages/npm-packages/runtime/src/ruby_wasm_ui.rb
@@ -8,6 +8,7 @@ require_relative "ruby_wasm_ui/nodes_equal"
 require_relative "ruby_wasm_ui/template"
 require_relative "ruby_wasm_ui/utils"
 require_relative "ruby_wasm_ui/vdom"
+require_relative "ruby_wasm_ui/version"
 
 module RubyWasmUi
 end
--- packages/npm-packages/runtime/src/ruby_wasm_ui/version.rb
+++ packages/npm-packages/runtime/src/ruby_wasm_ui/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RubyWasmUi
-  VERSION = "0.1.0"
+  VERSION = "0.8.1"
 end
