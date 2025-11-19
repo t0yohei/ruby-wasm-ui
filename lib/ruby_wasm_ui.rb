3e59eb1940f7205131721f695dbabc574ac7ae95
t0yohei <inuneko690121@gmail.com>
2025-11-19 22:50:57 +0900

    refactor: move src to lib

--- lib/ruby_wasm_ui.rb
+++ lib/ruby_wasm_ui.rb
@@ -1,1 +1,14 @@
-../packages/npm-packages/runtime/src/ruby_wasm_ui.rb
+# frozen_string_literal: true
+
+require_relative "ruby_wasm_ui/app"
+require_relative "ruby_wasm_ui/component"
+require_relative "ruby_wasm_ui/dispatcher"
+require_relative "ruby_wasm_ui/dom"
+require_relative "ruby_wasm_ui/nodes_equal"
+require_relative "ruby_wasm_ui/template"
+require_relative "ruby_wasm_ui/utils"
+require_relative "ruby_wasm_ui/vdom"
+require_relative "ruby_wasm_ui/version"
+
+module RubyWasmUi
+end
