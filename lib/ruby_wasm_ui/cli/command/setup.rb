82f18877dac6d63647809b2ed5fc5e9738aecfea
t0yohei <inuneko690121@gmail.com>
2025-12-08 18:56:50 +0900

    update: Set ruby version for user local

--- lib/ruby_wasm_ui/cli/command/setup.rb
+++ lib/ruby_wasm_ui/cli/command/setup.rb
@@ -14,10 +14,21 @@ module RubyWasmUi
           log_info("Setting up ruby-wasm-ui project...")
           puts ""
 
+          # Check Ruby version
+          ruby_version = RUBY_VERSION.split('.').map(&:to_i)
+          major, minor = ruby_version[0], ruby_version[1]
+
+          if major < 3 || (major == 3 && minor < 2)
+            log_error("Ruby WASM requires Ruby 3.2 or higher. Current version: #{RUBY_VERSION}")
+            exit 1
+          end
+
+          ruby_version_str = "#{major}.#{minor}"
+
           # Build Ruby WASM
           log_info("Step 1/2: Building Ruby WASM...")
-          log_info("Running: bundle exec rbwasm build --ruby-version 3.4 -o ruby.wasm")
-          run_command("bundle exec rbwasm build --ruby-version 3.4 -o ruby.wasm")
+          log_info("Running: bundle exec rbwasm build --ruby-version #{ruby_version_str} -o ruby.wasm")
+          run_command("bundle exec rbwasm build --ruby-version #{ruby_version_str} -o ruby.wasm")
           log_success("✓ Ruby WASM build completed")
 
           # Update .gitignore
--- spec/ruby_wasm_ui/cli/command/setup_spec.rb
+++ spec/ruby_wasm_ui/cli/command/setup_spec.rb
@@ -24,8 +24,9 @@ RSpec.describe RubyWasmUi::Cli::Command::Setup do
     end
 
     it 'executes rbwasm build command' do
+      ruby_version = RUBY_VERSION.split('.')[0..1].join('.')
       expect(setup_instance).to receive(:run_command).with(
-        'bundle exec rbwasm build --ruby-version 3.4 -o ruby.wasm'
+        "bundle exec rbwasm build --ruby-version #{ruby_version} -o ruby.wasm"
       )
       setup_instance.run([])
     end
@@ -50,6 +51,58 @@ RSpec.describe RubyWasmUi::Cli::Command::Setup do
       expect { setup_instance.run([]) }.to output(
         /Updating .gitignore/
       ).to_stdout
+    end
+
+    context 'when Ruby version is less than 3.2' do
+      before do
+        allow(Kernel).to receive(:exit)
+      end
+
+      context 'when Ruby version is 3.1' do
+        before do
+          stub_const('RUBY_VERSION', '3.1.5')
+        end
+
+        it 'outputs error message and exits' do
+          expect { setup_instance.run([]) }.to output(
+            /\[ERROR\] Ruby WASM requires Ruby 3.2 or higher. Current version: 3.1.5/
+          ).to_stdout
+          expect(Kernel).to have_received(:exit).with(1)
+        end
+
+        it 'does not execute rbwasm build command' do
+          expect(setup_instance).not_to receive(:run_command)
+          setup_instance.run([])
+        end
+      end
+
+      context 'when Ruby version is 2.7' do
+        before do
+          stub_const('RUBY_VERSION', '2.7.8')
+        end
+
+        it 'outputs error message and exits' do
+          expect { setup_instance.run([]) }.to output(
+            /\[ERROR\] Ruby WASM requires Ruby 3.2 or higher. Current version: 2.7.8/
+          ).to_stdout
+          expect(Kernel).to have_received(:exit).with(1)
+        end
+      end
+    end
+
+    context 'when Ruby version is 3.2' do
+      before do
+        stub_const('RUBY_VERSION', '3.2.0')
+        allow(setup_instance).to receive(:run_command)
+        allow(setup_instance).to receive(:update_gitignore)
+      end
+
+      it 'executes rbwasm build command with correct version' do
+        expect(setup_instance).to receive(:run_command).with(
+          'bundle exec rbwasm build --ruby-version 3.2 -o ruby.wasm'
+        )
+        setup_instance.run([])
+      end
     end
   end
 end
